49648d33430d114d861814821a66258d38e940e7
Christos Galeos <x.gal@hotmail.com>
2018-07-09 12:53:19 +0300

    Actual initial commit.

---
+++ Assets/Scripts/Menu/TQExitApp.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TQExitApp : MonoBehaviour {
+    
+    public void ExitApp(){
+        Application.Quit();
+        Debug.Log("log out");
+    }
+}
