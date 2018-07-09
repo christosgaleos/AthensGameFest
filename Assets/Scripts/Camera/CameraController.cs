c0550f28ea4e622dc24a92e68a4d3183f69fb189
Christos Galeos <x.gal@hotmail.com>
2018-07-09 12:41:06 +0300

    Initial commit.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 targetPosition;
+    GameObject player1;
+    GameObject player2;
+
+    float Y_OFFSET = 0f;
+    // Use this for initialization
+    void Start()
+    {
+        player1 = GameObject.FindGameObjectWithTag("Player1");
+        player2 = GameObject.FindGameObjectWithTag("Player2");
+        targetPosition = CalculatePlayerCenter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = CalculatePlayerCenter();
+        transform.LookAt(targetPosition);
+    }
+
+    Vector3 CalculatePlayerCenter()
+    {
+        Vector3 targetPosition = new Vector3((player1.transform.position.x + player2.transform.position.x) / 2, (player1.transform.position.y + player2.transform.position.y / 2) + Y_OFFSET, (player1.transform.position.z + player2.transform.position.z) / 2);
+
+        return targetPosition;
+    }
+}
---
+++ Assets/Scripts/_Legacy/Editor/Bump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bump : MonoBehaviour {
+    public float FORCE;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FORCE = FORCE + FORCE * 0.001f;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        {
+            collision.rigidbody.AddForce(transform.forward * FORCE);
+        }
+        
+    }
+}
